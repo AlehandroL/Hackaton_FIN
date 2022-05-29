020d691f5d666127d7b597af4ebd6120d002d3bd
alejandroleiva <aleivailabaca@gmail.com>
2022-05-28 22:13:07 -0400

    including google api in venv

--- chat_commerce/urls.py
+++ chat_commerce/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.RequestListView.as_view(), name='request_list'),
     path('offers/', views.OfferListView.as_view(), name='offer_list'),
     path('accept_offer/', views.RequestListView.as_view(), name='accept_offer'),
+    path('make_offer/', views.make_offer, name='make_offer'),
 ]
