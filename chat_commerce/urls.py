3f43b48d8b8f0f0616bae09c05418bc2bba6f330
Ignacio Heck <iaheck@uc.cl>
2022-05-29 02:21:23 -0400

    envia cita por api google calendar

--- chat_commerce/urls.py
+++ chat_commerce/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from . import views, viewAcceptOffer
 
 app_name = 'chat_commerce'
 
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.RequestListView.as_view(), name='request_list'),
     path('offered_to_you/', views.OfferedToYou_ListView.as_view(), name='offerred_to_you'),
     path('your_offers/', views.YourOffers_ListView.as_view(), name='your_offers'),
-    path('accept_offer/', views.RequestListView.as_view(), name='accept_offer'),
+    path('accept_offer/<int:offerId>/', viewAcceptOffer.accept_offer, name='accept_offer'),
     path('make_offer/<int:id>/', views.OfferCreateView.as_view(), name='make_offer'),
 ]
---
+++ chat_commerce/viewAcceptOffer.py
@@ -0,0 +1,71 @@
+import datetime
+import os.path
+
+from django.shortcuts import get_object_or_404, render, redirect
+from django.views import generic
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.template.defaultfilters import slugify
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from .models import Request, Offer
+
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+def accept_offer(receivedRequest, offerId):
+    offer = Offer.objects.get(pk=offerId)
+    request = Request.objects.get(pk=offer.id)
+    print (offer.User.username)
+    print (request)
+
+    #print(f'hola {self.Request.User.username}')
+    event = {
+        'summary': 'Chat: Ignacio',
+        'description': 'Hora de chat de servicio al cliente. Para pedir un cambio de hora, haz clic acá',
+        'start': {
+            'dateTime': '2022-05-29T08:30:00-04:00',
+            'timeZone': 'America/Santiago',
+        },
+        'end': {
+            'dateTime': '2022-05-29T09:30:00-04:00',
+            'timeZone': 'America/Santiago',
+        },
+        'attendees': [
+            {'email': 'iaheck@uc.cl'},
+            {'email': 'ignheck@gmail.com'},
+        ]
+    }
+    
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    #if os.path.exists('token.json'):
+    #    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        #event = service.events().insert(calendarId='primary', body=event, sendUpdates='all').execute()
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+    print ('Event created: %s' % (event.get('htmlLink')))
+    return HttpResponse("""<html><script>window.location.replace('/');</script></html>""")
